630f4267c02f533825ba97bc911f9aad86f51ee7
AQIBYASEENBHAT <AQIBYASEENBHAT@GMAIL.COM>
2022-06-15 13:08:35 +0530

    final commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let fname = document.getElementById("fname");
-let lname =document.getElementById("lname");
-let email =document.getElementById("email");
-
+let lname = document.getElementById("lname");
+let email = document.getElementById("email");
+let error = document.getElementById("error");
 
 // document.getElementById("form").addEventListener("click",function(){
     // 
@@ -12,9 +12,12 @@ document.getElementById("btn").addEventListener('click',function(e){
     e.preventDefault();
     if(fname.value != 0 && lname.value != 0 && email.value != 0){
     document.getElementById("div1").innerText=`${fname.dataset.fname} : ${fname.value}\n ${lname.dataset.lname} : ${lname.value} \n ${email.dataset.email} : ${email.value}`;
+    error.style.color="yellowgreen";
+    error.innerHTML="Data Captured Succesfully!!";
+
     }
     else{
-        document.getElementById("error").innerHTML="All Fields are Required!!";
+        error.innerHTML="All Fields are Required!!";
         document.getElementsByTagName("input").style.borderColor="red";
         
 
